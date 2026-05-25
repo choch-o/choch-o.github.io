2994caebbb6042a9cee9dfc29eee0e10389f1381
Hyunsung Cho <hscho1226@gmail.com>
2026-05-25 14:52:36 -0700

    Updates

---
+++ precache-manifest.434915f95037b885bbea7c0a38895e5d.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = [
+  {
+    "revision": "46c7e5fb72b667b37670",
+    "url": "/static/js/main.46c7e5fb.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.42ac5946.js"
+  },
+  {
+    "revision": "4875b2270247bd0b238d",
+    "url": "/static/js/2.4875b227.chunk.js"
+  },
+  {
+    "revision": "f1da6fa93f7b43b0210665c89bbed744",
+    "url": "/index.html"
+  }
+];
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.2cd0c3fe0afc2093272151cdadfcce55.js"
+  "/precache-manifest.434915f95037b885bbea7c0a38895e5d.js"
 );
 
 workbox.clientsClaim();
